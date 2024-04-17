6a77614150bb9e4538fe2c6f2b68f1a35c124193
ds-pavitra <pavitra.dsolutions@gmail.com>
2024-04-17 11:37:02 +0530

    initial commit

---
+++ mailer/forms_settings.php
@@ -0,0 +1,8 @@
+<?php
+	/* Forms settings */
+	$subj = "New message from the site 'Broadway9 Automation - Innovation Digital Solutions'"; //letter subject
+	$to = 'your_email@domain.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'Broadway9 Automation - Innovation Digital Solutions'; // Your company name
+	$charset = 'UTF-8';   
+?>
